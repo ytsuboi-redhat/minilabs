3b761f52911fdf0a23efc980e2438ade443824d8
Yosuke Tsuboi <ytsuboi@redhat.com>
2019-01-07 16:50:35 +0900

    commit

--- todo-frontend/src/components/Todo.vue
+++ todo-frontend/src/components/Todo.vue
@@ -1,34 +1,44 @@
 <template>
-  <div class="todo-container">
+  <div class="container todo-container">
     <h1>todo-app</h1>
-    <div id="result">{{ message }}</div>
-    <form v-on:submit.prevent="getAll">
-      <button id="submitButton" type="submit" name="button" class="btn btn-primary col-sm-3">click me!!</button>
-    </form>
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col"></th>
-          <th scope="col">title</th>
-          <th scope="col">status</th>
-          <th scope="col">description</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(todo, index) in todos" :key="todo.todoId">
-          <td class="index">{{ index }}</td>
-          <td class="title">
-            <input v-model="todo.title" placeholder="title">
-          </td>
-          <td class="status">
-            <input v-model="todo.status" placeholder="status">
-          </td>
-          <td class="desc">
-            <input v-model="todo.description" placeholder="description">
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="row p-3">
+      <div class="col-sm">
+        <form v-on:submit.prevent="getAll">
+          <button id="submitButton" type="submit" name="button" class="btn btn-primary col-sm-3">show todos</button>
+        </form>
+      </div>
+    </div>
+    <div class="row p-3">
+      <div class="col-sm">
+        <div id="result">{{ message }}</div>
+        <div v-bind:class="tabVisible">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">title</th>
+                <th scope="col">status</th>
+                <th scope="col">description</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="todo in todos" :key="todo.todoId">
+                <td class="index">{{ todo.todoId }}</td>
+                <td class="title">
+                  <input class="form-control" v-model="todo.title" placeholder="title">
+                </td>
+                <td class="status">
+                  <input class="form-control" v-model="todo.status" placeholder="status">
+                </td>
+                <td class="desc">
+                  <textarea class="form-control" rows="5" v-model="todo.description" placeholder="description"></textarea>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -42,6 +52,7 @@ export default {
   },
   data () {
     return {
+      tabVisible: 'd-none',
       message: '',
       todos: []
     }
@@ -51,10 +62,12 @@ export default {
       const service = new TodoService()
       service.getAll()
         .then(response => {
+          this.tabVisible = 'd-block'
           this.todos = response.data
         })
         .catch(() => {
-          this.message = 'error'
+          this.tabVisible = 'd-block'
+          // this.message = 'error'
         })
     }
   }
